0cc7ad1361395618049da0453c9c049db13decd2
Kreagentle <ponwrt@gmail.com>
2025-04-14 17:55:46 +0100

    Move main to the different folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Kreqgentle/booking_service/pkg/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
