a8cfcba190d0a37a04160c0937b5c73f7db853a5
Kreagentle <ponwrt@gmail.com>
2025-04-15 17:29:13 +0100

    Add layouts

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, tmplt string) {
-	parsedTmpl, _ := template.ParseFiles("./templates" + tmplt)
+	parsedTmpl, _ := template.ParseFiles("./templates"+tmplt, "./templates/base.layout.tmpl")
 	err := parsedTmpl.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template %w", err)
