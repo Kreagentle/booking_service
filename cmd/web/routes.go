5d0b0ef64575575ff95227a08fa537490ba7f088
Kreagentle <ponwrt@gmail.com>
2025-04-24 09:08:25 +0100

    Add chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,20 @@ import (
 
 	"github.com/Kreqgentle/booking_service/pkg/config"
 	"github.com/Kreqgentle/booking_service/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	/*mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Rpstr.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Rpstr.About))
+	mux.Get("/about", http.HandlerFunc(handlers.Rpstr.About))*/
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Rpstr.Home)
+	mux.Get("/about", handlers.Rpstr.About)
 
 	return mux
 }
