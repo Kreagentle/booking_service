989cbef16a80fe4c4266d3581bebad5701bb6357
Kreagentle <ponwrt@gmail.com>
2025-05-01 11:27:19 +0100

    Enabling static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,5 +23,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Rpstr.Home)
 	mux.Get("/about", handlers.Rpstr.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
